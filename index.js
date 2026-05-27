5a230bed8047bcc083966aeed6e7fca19e36cf85
Martin Sonda <martindanielsonda@gmail.com>
2026-05-27 02:30:05 -0300

    feat: add get products

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const method = process.argv[2];
 const endpoint = process.argv[3];
 
 const partes = endpoint.split("/");
-
 const [resource, id] = partes;
 
-console.log("Método:", method);
-console.log("Endpoint:", endpoint);
+switch (method) {
+  case "GET":
+    if (!id) {
+      getProducts();
+    }
+    break;
 
-console.log("Recurso:", resource);
-console.log("ID:", id);
+
+  default:
+    console.log("Comando no reconocido");
+}
 
 
+async function getProducts() {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products"
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log("Error al obtener productos");
+  }
+}
