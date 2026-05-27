3eae57e8f18fdb2e81a9811642118168f94ab575
Martin Sonda <martindanielsonda@gmail.com>
2026-05-27 03:15:42 -0300

    feat: add post product

--- index.js
+++ index.js
@@ -2,47 +2,84 @@ const method = process.argv[2];
 const endpoint = process.argv[3];
 
 const partes = endpoint.split("/");
+
 const [resource, id] = partes;
 
+const [, , , , ...args] = process.argv;
+
 switch (method) {
-  case "GET":
-    if (!id) {
-      getProducts();
-    } else {
-        getProductById(id);
-    }
-    break;
+    case "GET":
+        if (!id) {
+            getProducts();
+        } else {
+            getProductById(id);
+        }
+        break;
 
+    case "POST":
+        createProduct();
+        break;
 
-  default:
-    console.log("Comando no reconocido");
+    default:
+        console.log("Comando no reconocido");
 }
 
 
 async function getProducts() {
-  try {
-    const response = await fetch(
-      "https://fakestoreapi.com/products"
+    try {
+        const response = await fetch(
+        "https://fakestoreapi.com/products"
     );
 
     const data = await response.json();
 
     console.log(data);
-  } catch (error) {
-    console.log("Error al obtener productos");
-  }
+    } catch (error) {
+        console.log("Error al obtener productos");
+    }
 }
 
 async function getProductById(productId) {
-  try {
-    const response = await fetch(
-      `https://fakestoreapi.com/products/${productId}`
+    try {
+        const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`
     );
 
     const data = await response.json();
 
     console.log(data);
-  } catch (error) {
-    console.log("Error al obtener producto");
-  }
+    } catch (error) {
+        console.log("Error al obtener producto");
+    }
+}
+
+
+async function createProduct() {
+    try {
+        const [title, price, category] = args;
+
+        const newProduct = {
+            title,
+            price: Number(price),
+            category
+        };
+
+    const response = await fetch(
+        "https://fakestoreapi.com/products",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newProduct)
+        }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    } catch (error) {
+        console.log("Error al crear producto");
+    }
 }
