c9403ccf2393f42feab25d2207b366848d10db24
Martin Sonda <martindanielsonda@gmail.com>
2026-05-27 01:59:38 -0300

    feat: agregar split endpoint

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const method = process.argv[2];
 const endpoint = process.argv[3];
 
+const partes = endpoint.split("/");
+
+const [resource, id] = partes;
+
 console.log("Método:", method);
 console.log("Endpoint:", endpoint);
+
+console.log("Recurso:", resource);
+console.log("ID:", id);
+
 
